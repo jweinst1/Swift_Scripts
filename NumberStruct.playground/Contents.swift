af1fecde0a6637be84f2bd24d861fac835564f87
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-02-24 10:49:41 -0800

    number struct finished

--- NumberStruct.playground/Contents.swift
+++ NumberStruct.playground/Contents.swift
@@ -8,12 +8,14 @@ struct Number {
     var current: [Character]
     var index:Int
     var last:Character
+    var initial:[Character]
     
     init(seq:String) {
         self.sequence = Array(seq.characters)
         self.current = [self.sequence[0]]
         self.index = 0
         self.last = self.sequence[self.sequence.count-1]
+        self.initial = [self.sequence[0]]
         
     }
     //prints the current value of the number
@@ -25,11 +27,62 @@ struct Number {
         print(printable)
     }
     
+    //increments the number once
     mutating func increment() ->Void {
-        //increments the number
+        var place = self.current.count-1
+        var incmode = true
+        while incmode {
+            if place == 0 && self.current[place] == self.last {
+                self.current[place] = self.sequence[0]
+                self.current.insert(self.sequence[0], atIndex: 0)
+                incmode = false
+            }
+            else if self.current[place] == self.last {
+                self.current[place] = self.sequence[0]
+                place--
+            }
+            else {
+                self.current[place] = self.sequence[self.sequence.indexOf(self.current[place])!+1]
+                incmode = false
+            }
+        }
+    }
+    //increments the number a multiple number of times.
+    mutating func incrementby(times:Int) ->Void {
+        for _ in 1...times {
+            self.increment()
+        }
     }
     
     mutating func decrement() ->Void {
-        //decrements the number
+        if self.current == self.initial {
+            print("Base Number Cannot decrement")
+        }
+        var place = self.current.count-1
+        var decmode = true
+        while decmode {
+            if place == 0 && self.current[place] == self.sequence[0] {
+                self.current.removeFirst()
+                decmode = false
+            }
+            else if self.current[place] == self.sequence[0] {
+                self.current[place] = self.last
+                place--
+            }
+            else {
+                self.current[place] = self.sequence[self.sequence.indexOf(self.current[place])!-1]
+                decmode = false
+            }
+        }
+    }
+    
+    mutating func decrementby(times:Int) ->Void {
+        for _ in 1...times {
+            self.decrement()
+        }
     }
 }
+
+
+
+
