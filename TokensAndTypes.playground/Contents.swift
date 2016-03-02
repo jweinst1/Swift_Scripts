42cd29c7ecee4e595e9efd87077da5a3021d17ba
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-02 14:18:51 -0800

    new token system

--- TokensAndTypes.playground/Contents.swift
+++ TokensAndTypes.playground/Contents.swift
@@ -23,3 +23,15 @@ var it = table[0] as! IntToken // take it out with cast and var ref
 it.add(table[1] as! IntToken) // add with cast; `it` is now IntToken(val:7)
 table[0] = it // put it back in (if you want to)
 print(table)
+
+//performs casting and adding of tokens
+func addright(var stack:[Any], first:Int, second:Int) -> [Any] {
+    var hold = stack[second] as! IntToken
+    hold.add(stack[first] as! IntToken)
+    stack[second] = hold
+    return stack
+}
+print(addright(table, first:0, second:1))
+//[IntToken(value: 7), IntToken(value: 11)]
+
+
